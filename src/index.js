9bed8f56350a1380309b49c3b294ee1649a4aa22
Seva <seva.leonov@hotmail.com>
2023-12-01 18:28:15 +0700

    Add createDomElements function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import "./styles.css";
 
-console.log("test");
+const DOMTree = [
+  {
+    elementType: "div",
+    elementClass: "main",
+    elementId: "",
+    childElements: [
+      {
+        elementType: "div",
+        elementClass: "secondary",
+        elementId: "",
+      },
+      {
+        elementType: "div",
+        elementClass: "secondary",
+        elementId: "",
+      },
+    ],
+  },
+];
+
+const body = document.querySelector("body");
+
+function createDomElements(tree, parentElement) {
+  tree.forEach((obj) => {
+    const element = document.createElement(obj.elementType);
+    element.classList.add(obj.elementClass);
+    parentElement.append(element);
+
+    if (obj.childElements) {
+      createDomElements(obj.childElements, element);
+    }
+  });
+}
+
+createDomElements(DOMTree, body);
