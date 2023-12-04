840b2fc97028d7ee2f129d6d6cde0caaa3352577
Seva <seva.leonov@hotmail.com>
2023-12-04 20:07:17 +0700

    add styles for search button

--- src/domHandler.js
+++ src/domHandler.js
@@ -20,7 +20,7 @@ const createDom = (() => {
                       {
                         elementType: "label",
                         elementClass: "",
-                        elementTextContent: "Search",
+                        elementTextContent: "",
                         elementAttributes: [
                           {
                             attributeName: "for",
@@ -34,7 +34,7 @@ const createDom = (() => {
                         elementAttributes: [
                           {
                             attributeName: "type",
-                            attributeValue: "text",
+                            attributeValue: "search",
                           },
                           {
                             attributeName: "id",
@@ -42,7 +42,18 @@ const createDom = (() => {
                           },
                           {
                             attributeName: "placeholder",
-                            attributeValue: "search location",
+                            attributeValue: "Search location",
+                          },
+                        ],
+                      },
+                      {
+                        elementType: "button",
+                        elementClass: "search-submit-button",
+                        elementInnerHTML: `<svg class="svg-inline-submit" aria-hidden="true" focusable="false" data-prefix="far" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="white" d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"></path></svg>`,
+                        elementAttributes: [
+                          {
+                            attributeName: "type",
+                            attributeValue: "submit",
                           },
                         ],
                       },
@@ -385,6 +396,9 @@ const createDom = (() => {
       }
       if (obj.elementTextContent) {
         element.textContent = obj.elementTextContent;
+      }
+      if (obj.elementInnerHTML) {
+        element.innerHTML = obj.elementInnerHTML;
       }
       parentElement.append(element);
 
